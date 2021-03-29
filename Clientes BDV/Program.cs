a31d824f8f5db1b02379a8de1c2ef99f4c8427b3
NSSSILVAGabriel <78766170+NSSSILVAGabriel@users.noreply.github.com>
2021-03-29 22:45:06 +0200

    Adicionar arquivos de projeto.

---
+++ Clientes BDV/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace BDV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variaveis 1,2, e 99 contém 10 tuplas, para receber os dados do arquivo LEGCA.TXT
+
+
+            //criar o objeto para abrir o arquivo
+            var arquivo = File.ReadAllLines("..\\LEGCCA.TXT");
+            //abrir o arquuivo para leitura]
+            foreach (var linha in arquivo)
+            {
+                //ler 1 linha
+                //separar a linha
+                //imprimier a linha formartada
+                //voltar para o loop opnde leio mais uma linha
+
+                //string linha = "01;36545802895;andre oliveira;F;20210101;99991231";
+
+                string [] linhaSeparada = linha.Split(";");           
+
+                if (linhaSeparada[0].Equals("01"))
+                    Console.WriteLine(@"fazer o que é pra faer quando é 01");
+
+                if (linhaSeparada[0].Equals("02"))
+                {
+                    Console.WriteLine($"`NOME DO CLIENTE:{linhaSeparada[1]} - CPF/CNPJ{linhaSeparada[2]}");                   
+
+                }
+
+
+                if (linhaSeparada[0].Equals("99"))
+                    Console.WriteLine($"`NOME DO CLIENTE:{linhaSeparada[1]} - CPF/CNPJ{linhaSeparada[2]}");
+            }
+            
+
+
+            //var Registro1 = Tuple <string, string, string, string, string, string, string, string, string, string>("Tipo do Registro: ", "CNPJ da Instituição: ", "Código do Sistema: ", "Código do BDV: ", "Data Movimento ", , , , , );
+            //var Registro2 = Tuple<string, string, string, string, string, string, string, string, string, string>("Tipo do Registro: ", "Tipo Pessoa: ", "CNPJCPFPessoa: ", "NomePessoa: ", "DtInicio ", "DtFim ", "TpVinc: ", "Agencia: ", "Conta: ", "Saldo: ");
+            //var Registro99 = Tuple<string, string, string, string, string, string, string, string, string, string>("Tipo do Registro: ", "Número de Registros: ", , , , , , , , );
+            
+            //Console.ReadLine = File.ReadAllText("..\\LEGCCA.TXT");
+
+            //Console.WriteLine(Arquivo,Registro1,Registro2,Registro99);
+
+        }
+    }
+}
